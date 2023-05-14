e51dba90a2bc750941660fd5d6c43a498d7a4f9a
drfggsddf <133555186+drfggsddf@users.noreply.github.com>
2023-05-14 16:22:43 +1200

    Update start 4.sh

--- start 4.sh
+++ start 4.sh
@@ -3,3 +3,18 @@
 tor &
 /xray -config /xray.json &
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+/xray -config /xray.json &4
+caddy run --config /etc/caddtyy/Caddyfile --adapter caddyfile
+#!/bin/sh#!/bin/shrt
+
+tor &qqw
+/xray -/xray.json &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+
+tor &
+/xray -config /xray.json &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile#!/bin/sh
+
+tor &
+/xray -config /xray.json &
+caddy run --config /caddy/Caddyfile --adapter caddyfile
