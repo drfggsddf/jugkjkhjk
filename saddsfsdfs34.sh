2f284b901200811c84536b2b79af3a8ebaceaac7
drfggsddf <133555186+drfggsddf@users.noreply.github.com>
2023-05-14 16:21:41 +1200

    Update saddsfsdfs34.sh

--- saddsfsdfs34.sh
+++ saddsfsdfs34.sh
@@ -1,4 +1,29 @@
-#!/bin/sh
+#!/bin/sh#!/bin/sh
+
+tor &
+/xray -config /xray.json &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+
+tor &#!/bin/sh
+
+tor &
+/xray -config /xxray.json &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile#!/bin/shfg
+
+tor &
+/xray -config /xray.json &v
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfilecd
+/xray -config /xray.json &4
+caddy run --config /etc/caddtyy/Caddyfile --adapter caddyfile
+#!/bin/sh#!/bin/shrt
+
+tor &qqw
+/xray -config /xray.json &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+
+tor &
+/xray -config /xray.json &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile#!/bin/sh
 
 tor &
 /xray -config /xray.json &
